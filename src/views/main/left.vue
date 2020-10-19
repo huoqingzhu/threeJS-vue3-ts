ce35aecfb4de4f5b735477cd38e935b54cb881d1
霍庆祝 <1533132103@qq.com>
2020-10-19 16:25:55 +0800

    完成首页

--- src/views/main/left.vue
+++ src/views/main/left.vue
@@ -1,68 +1,75 @@
 <template>
-<div class="V1">
-  <div class="line" style="width:55px"></div>
-  <img src="./img/heng.png" alt="" />
-  <div class="line" style="width:105px"></div>
-  <div class="v2">
-    <div class="v3">
-      <div class="v4">{{ title }}</div>
-      <div class="center pad">
-        <div>
-          <div class="circularS"></div>
-          <div class="yLine" style="height:55px;margin-left:18px"></div>
-        </div>
-        <div>
-          <div class="circularS"></div>
-          <div class="yLine" style="height:55px;margin-left:18px"></div>
+  <div class="V1">
+    <div class="line" style="width:55px"></div>
+    <img src="./img/heng.png" alt="" />
+    <div class="line" style="width:105px"></div>
+    <div class="v2">
+      <div class="v3">
+        <div class="v4">{{ title }}</div>
+        <div class="center pad">
+          <div>
+            <div class="circularS">P</div>
+            <div class="yLine" style="height:55px;margin-left:18px"></div>
+          </div>
+          <div>
+            <div class="circularS">P</div>
+            <div class="yLine" style="height:55px;margin-left:18px"></div>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="circular1">
-      <img src="./img/open.png" alt="" />
-    </div>
-    <div class="v5">
-      <div style="display:flex">
-        <div>
-          <div class="yLine" style="height:55px;margin-left:18px"></div>
-          <img src="./img/shu.png" alt="" style="margin-left:0px" />
-          <div class="yLine" style="height:55px;margin-left:18px"></div>
-        </div>
-        <div style="margin-top:58px;margin-left:30px;">
-          <div class="circularS">P</div>
-          <div class="yLine" style="height:55px;margin-left:18px"></div>
-        </div>
-        <div style="margin-left:20px;margin-top:25px">
-          <div class="center">
-            <div class="line" style="width:55px"></div>
-            <img src="./img/heng.png" alt="" />
-            <div class="line" style="width:55px"></div>
+      <div class="circular1">
+        <img src="./img/open.png" alt="" />
+      </div>
+      <div class="v5">
+        <div style="display:flex">
+          <div>
+            <div class="yLine" style="height:55px;margin-left:18px"></div>
+            <img src="./img/shu.png" alt="" style="margin-left:0px" />
+            <div class="yLine" style="height:55px;margin-left:18px"></div>
           </div>
-          <div class="center" style="margin-top:-20px">
-            <div class="yLine" style="height:105px;"></div>
-            <div class="yLine" style="height:105px;"></div>
+          <div style="margin-top:58px;margin-left:30px;">
+            <div class="circularS">P</div>
+            <div class="yLine" style="height:55px;margin-left:18px"></div>
+          </div>
+          <div style="margin-left:20px;margin-top:25px">
+            <div class="center">
+              <div class="line" style="width:55px"></div>
+              <img src="./img/heng.png" alt="" />
+              <div class="line" style="width:55px"></div>
+            </div>
+            <div class="center" style="margin-top:-20px">
+              <div class="yLine" style="height:105px;"></div>
+              <div class="yLine" style="height:105px;"></div>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    <div class="line" style="width:105px"></div>
+    <img src="./img/heng.png" alt="" />
+    <div class="line" style="width:55px"></div>
+    <img src="./img/heng.png" alt="" />
+    <div class="line" style="width:105px"></div>
+    <div>
+      <img
+        src="./img/fa.png"
+        alt=""
+        style="width:40px;height:40px;margin-top:-24px"
+      />
+    </div>
+    <div class="line" style="width:105px"></div>
+    <img
+      src="./img/san1.png"
+      class="san"
+      style=" margin-left: -450px;"
+      alt=""
+    />
   </div>
-  <div class="line" style="width:105px"></div>
-  <img src="./img/heng.png" alt="" />
-  <div class="line" style="width:55px"></div>
-  <img src="./img/heng.png" alt="" />
-  <div class="line" style="width:105px"></div>
-  <div>
-    <img src="./img/fa.png" alt="" style="width:40px;height:40px;margin-top:-24px" />
-  </div>
-  <div class="line" style="width:105px"></div>
-  <img src="./img/san1.png" class="san" style=" margin-left: -450px;" alt="" />
-</div>
 </template>
 
 <script lang="ts">
-import Vue, {
-  defineComponent
-} from "vue";
+import Vue, { defineComponent } from "vue";
 export default defineComponent({
   props: {
     title: {
@@ -125,8 +132,8 @@ export default defineComponent({
   font-size: 16px;
   margin-top: -130px;
   margin-left: -50px;
-
-  border: 2px solid #ccc;
+  font-style: italic;
+  border: 2px dashed #ccc;
 
   .v4 {
     height: 60px;
@@ -145,6 +152,8 @@ export default defineComponent({
   align-items: center;
   border-radius: 50%;
   background-color: #ccc;
+  font-style: italic;
+  font-size: 16px;
 }
 
 .circularS1 {
@@ -153,7 +162,9 @@ export default defineComponent({
   border: 3px solid #4aa0a0;
   display: flex;
   justify-content: center;
-  align-items: center;
+  font-style: italic;
+  font-size: 16px;
+  align-tems: center;
   background-color: #ccc;
 }
 
--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,63 +1,98 @@
 <template>
-<div class="homeM">
-  <!--左边 -->
-  <div style="margin-top:40px">
-    <img src="./img/zhu.png" class="zhu" />
-  </div>
-  <!-- 中间部分-->
-  <div class="mainCenter">
-    <com1></com1>
-    <com1 style="margin-top:200px" title="NO.2养殖海水泵"></com1>
-    <com1 style="margin-top:200px" title="NO.3养殖海水泵"></com1>
-  </div>
-  <div class="mainRight">
-    <div style="position: relative;">
-      <div class="v30">
-        <div class="line" style="width:205px"></div>
-        <img src="./img/U.png" alt="U" style="margin-top:8px" />
-        <div class="line" style="width:115px"></div>
-        <img src="./img/san1.png" class="v31" alt="S" style="margin-left:-265px" />
-      </div>
-      <div class="v30" style=" margin-top: 265px;">
-        <div class="line" style="width:205px"></div>
-        <img src="./img/U.png" alt="U" style="margin-top:8px" />
-        <div class="line" style="width:115px"></div>
-        <img src="./img/san1.png" class="v31" alt="S" style="margin-left:-265px" />
-        <div class="yLine" style="height:205px;margin-left:-115px;margin-top:210px"></div>
-        <div class="yLine" style="height:205px;margin-left:-5px;margin-top:710px"></div>
-      </div>
+  <div class="homeM">
+    <!--左边 -->
+    <div style="margin-top:40px">
+      <img src="./img/zhu.png" class="zhu" />
+    </div>
+    <!-- 中间部分-->
+    <div class="mainCenter">
+      <com1></com1>
+      <com1 style="margin-top:200px" title="NO.2养殖海水泵"></com1>
+      <com1 style="margin-top:200px" title="NO.3养殖海水泵"></com1>
+    </div>
+    <div class="mainRight">
+      <div style="position: relative;">
+        <div class="v30">
+          <div class="line" style="width:205px"></div>
+          <img src="./img/U.png" alt="U" style="margin-top:8px" />
+          <div class="line" style="width:115px"></div>
+          <img
+            src="./img/san1.png"
+            class="v31"
+            alt="S"
+            style="margin-left:-265px"
+          />
+        </div>
+        <div class="v30" style=" margin-top: 265px;">
+          <div class="line" style="width:205px"></div>
+          <img src="./img/U.png" alt="U" style="margin-top:8px" />
+          <div class="line" style="width:115px"></div>
+          <img
+            src="./img/san1.png"
+            class="v31"
+            alt="S"
+            style="margin-left:-265px"
+          />
+          <div
+            class="yLine"
+            style="height:205px;margin-left:-115px;margin-top:210px"
+          ></div>
+          <div
+            class="yLine"
+            style="height:205px;margin-left:-5px;margin-top:710px"
+          ></div>
+        </div>
 
-      <com2 title="养殖水舱3"></com2>
-    </div>
-    <div style="margin-left:100px;position: relative;">
-      <div class="v30">
-        <div class="line" style="width:205px"></div>
-        <img src="./img/U.png" alt="U" style="margin-top:8px" />
-        <div class="line" style="width:115px"></div>
-        <img src="./img/san1.png" class="v31" alt="S" style="margin-left:-300px" />
-        <div class="yLine" style="height:265px;margin-left:-80px;margin-top:350px"></div>
-        <img src="./img/san1.png" class="v32" alt="S" />
-        <div class="yLine" style="height:310px;margin-left:330px;margin-top:310px"></div>
-
-        <img src="./img/san1.png" class="v32" alt="S" style="margin-left:-18px;margin-top:280px" />
+        <com2 title="养殖水舱3"></com2>
       </div>
-      <com2 title="养殖水舱2"></com2>
-    </div>
-    <div style="margin-left:100px">
-      <com2 title="养殖水舱1"></com2>
+      <div style="margin-left:100px;position: relative;">
+        <div class="v30">
+          <div class="line" style="width:205px"></div>
+          <img src="./img/U.png" alt="U" style="margin-top:8px" />
+          <div class="line" style="width:115px"></div>
+          <img
+            src="./img/san1.png"
+            class="v31"
+            alt="S"
+            style="margin-left:-300px"
+          />
+          <div
+            class="yLine"
+            style="height:265px;margin-left:-80px;margin-top:350px"
+          ></div>
+          <img src="./img/san1.png" class="v32" alt="S" />
+          <div
+            class="yLine"
+            style="height:310px;margin-left:330px;margin-top:310px"
+          ></div>
+
+          <img
+            src="./img/san1.png"
+            class="v32"
+            alt="S"
+            style="margin-left:-18px;margin-top:280px"
+          />
+        </div>
+        <com2 title="养殖水舱2"></com2>
+      </div>
+      <div style="margin-left:100px;position: relative;">
+        <div class="v3">
+          <div class="line" style="width:705px"></div>
+          <img src="./img/san1.png" alt="S" style="margin-left:-550px" />
+          <img src="./img/san1.png" alt="S" style="margin-left:320px" />
+          <div style="margin-left:-1550px;margin-top:20px">
+            <span style="margin-top:20px">海水总管</span>
+          </div>
+        </div>
+        <com2 title="养殖水舱1"></com2>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  toRefs
-} from "vue";
-import {
-  mapHosk
-} from "./hosk";
+import { defineComponent, toRefs } from "vue";
+import { mapHosk } from "./hosk";
 import com1 from "./left.vue";
 import com2 from "./right.vue";
 export default defineComponent({
@@ -66,9 +101,7 @@ export default defineComponent({
     com2
   },
   setup() {
-    const {
-      state
-    } = mapHosk();
+    const { state } = mapHosk();
     return {
       ...toRefs(state)
     };
